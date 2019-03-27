f5ef9ce9fc0558188884e79a3f483380f87d85d4
brandonconover90@gmail.com <brandonconover90@gmail.com>
2019-03-26 20:56:23 -0600

    added basic chat feature

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,137 +1,6 @@
 $(document).ready(function () {
     M.AutoInit();
     // FIREBASE CONFIG
-    // 
-    //     /* <script src="https://www.gstatic.com/firebasejs/5.9.1/firebase.js"></script> */
-    //
-    // var config = {
-    //     apiKey: "AIzaSyD6vRPaTQwhxm4Zs-oa7Rw8eyS2mnnCR84",
-    //     authDomain: "hikeit-34330.firebaseapp.com",
-    //     databaseURL: "https://hikeit-34330.firebaseio.com",
-    //     projectId: "hikeit-34330",
-    //     storageBucket: "",
-    //     messagingSenderId: "956706165596"
-    // };
-    // firebase.initializeApp(config);
-
-    // DATABASE VARIABLES
-    // var database = firebase.database();
-    // var chatData = database.ref("/chat/");
-    // var connectionsRef = database.ref("/connections");
-    // var connectedRef = database.ref(".info/connected");
-
-    // LISTENERS FOR FIREBASE
-    // 1. client connection changes
-    // 
-    // 2. disconnection events
-    // 
-    // 3. chat snapshot
-    // 
-    // 4. local data snapshot at page load and other value changes
-    // 
-
-    // GOOGLE API INFORMATION
-    var GEOcity;
-    var GEOkey = "AIzaSyCRZmQJcBVO85oD5CSKZSc80BAtfvqD9HU";
-    var GEOquery = `https://maps.googleapis.com/maps/api/geocode/json?address=${GEOcity}&key=${GEOkey}`;
-
-    $.ajax({
-        url: GEOquery,
-        method: "GET"
-    }).then(function (response) {
-        var GEOresult = response.data;
-        console.log(GEOresult);
-        cityLat = GEOresult.geometry.location.lat;
-        cityLon = GEOresult.geometry.location.lng;
-    });
-
-    // HIKEPROJECT API INFORMATION
-    var cityLat;
-    var cityLon;
-    var HIKEkey = "";
-    var HIKEquery = `https://www.hikingproject.com/data/get-trails?lat=${cityLat}&lon=${cityLon}&key=${HIKEkey}`;
-
-    $.ajax({
-        url: HIKEquery,
-        method: "GET"
-    }).then(function (response) {
-        var hikeResult = response.data;
-        console.log(hikeResult);
-
-        for (let i = 0; i < 10; i++) {
-            var img = $("<img>");
-            img.addClass("src", hikeResult[i].imgMedium);
-
-
-
-        }
-
-    })
-
-    //TOFINISH: Google Calendar API info: 
-    // var;
-    // var googleCalendarkey = "AIzaSyCRZmQJcBVO85oD5CSKZSc80BAtfvqD9HU";
-    // var goocalquery = `https://${GEOkey}`;
-
-    // $.ajax({
-    //     url: goocalquery,
-    //     method: "GET"
-    // }).then(function (response) {
-    //     var gooCalResult = response.data;
-    //     console.log(gooCalResult);
-
-    // });
-
-    // ----------------------------------------------------------------------------------------------
-    // REQUIREMENTS REMINDER: 2 new technologies
-    //      (1) DOING: Materialize: Dates-> Under Pickers
-    //      (2) TODO: Google Fonts (for making it pretty :) 
-    //              (I, brandon, am willing to take charge of this if no one objects)
-
-    // FORM FOR CREATING ACCOUNT
-    // input # or . :
-    // on submit click grab input .val().trim() = username & password/email
-    // submit button listener
-    // ("SUBMIT-BTN").on("click", function(){
-
-    // CODE GOES HERE 
-
-    // })
-
-    // Form for searching for hike: 
-    // Enter:
-    //      Zipcode 
-    //      Date
-    //      maxDistance
-    //      Sort by quality or distance - toggle 
-    //          (TODO: How does this actually work? Should this feature be included in initial form or results?)
-    //              (perhaps it could be a checkbox?)
-    //      minLength
-    //      minStars
-    //      
-    // 
-
-    // Select on-click
-    //      api call 
-    // variables from form: zip, date, distance, (sort?), minlength, stars
-    // include method(s) for: conditions, 
-    // variables for weatherAPI: zip && date
-    // variables for hikingAPI: distance, (sort?), minlength, stars
-    // var apiKEY = ????
-    // var queryURL = https://www.hikingproject.com/data/get-trails?
-
-    // 
-
-
-
-
-    // --------------------------------------------------------------------------------------------
-    // Possibilities to expand: Include a feature to add hike event to Google calendar 
-    // Google calendar apiKey = AIzaSyBBtB6jSwzA56kq_8H6kxqA-T9xTz_ofDQ
-
-
-    M.AutoInit();
-    // FIREBASE CONFIG
 
     var config = {
         apiKey: "AIzaSyD6vRPaTQwhxm4Zs-oa7Rw8eyS2mnnCR84",
@@ -143,21 +12,45 @@ $(document).ready(function () {
     };
     firebase.initializeApp(config);
 
-    // DATABASE VARIABLES
-    var database = firebase.database();
-    var auth = firebase.auth();
-    // var chatData = database.ref("/chat/");
-    // var connectionsRef = database.ref("/connections");
-    // var connectedRef = database.ref(".info/connected");
+    // **************** CHAT FUNCTION:start ***********************
+    //   var user = firebase.auth().signInAnonymously();
+    //   firebase.auth().onAuthStateChanged(function (user) {
+    //       if (user) {
+    //           // User is signed in.
+    //           var isAnonymous = user.isAnonymous;
+    //           user_id = user.uid;
+    //       } else {
+    //           // User is signed out.
+    //       }
+    //   });
 
-    // LISTENERS FOR FIREBASE
-    // 1. client connection changes
-    // 
-    // 2. disconnection events
-    // 
-    // 3. chat snapshot
-    // 
-    // 4. local data snapshot at page load and other value changes
+    //   // get firebase database reference...
+    //   var db_ref = firebase.database().ref('/');
+    //   db_ref.on('child_added', function (data) {
+    //       var type;
+    //       if (data.val().user_id == user_id) {
+    //           type = "sent";
+    //       } else {
+    //           type = "replies";
+    //       }
+    //       $('<li class="' + type + '"><p>' + data.val().message + '</p></li>').appendTo($(
+    //           '.messages ul'));
+    //       $('.message-input input').val(null);
+    //       $('.contact.active .preview').html('<span>You: </span>' + data.val().message);
+    //       $(".messages").animate({
+    //           scrollTop: $(".messages")[0].scrollHeight
+    //       }, 0);
+    //   });
+
+    //   function writeUserData(message) {
+    //       db_ref.push({
+    //           user_id: user_id,
+    //           message: message
+    //       });
+    //   }
+    // **************** CHAT FUNCTION:end ***********************
+
+
 
 
 
