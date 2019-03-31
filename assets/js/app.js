a3c9a0558713576957f9306739a948ed829762c5
jessiBramwell <bramibly@gmail.com>
2019-03-31 13:43:37 -0600

    changed momentjs to avoid depreciation error

--- assets/js/app.js
+++ assets/js/app.js
@@ -340,7 +340,7 @@ $(document).ready(function () {
 
   var outOfDate = function (ref, data, val) {
     var expires = moment();
-    var check = moment(val.date);
+    var check = moment(val.date, "MMMM Do YYYY, h:mm a");
 
     if (expires > check) {
       // console.log(val.name + " out of date");
